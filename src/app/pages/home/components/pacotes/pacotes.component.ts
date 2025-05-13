d0e9a593252b3c37d3fe592b959743700eb07746
Provera Samuel <proverasamuel2022@gmail.com>
2025-05-13 19:48:00 +0100

    fix, url

--- src/app/pages/home/components/pacotes/pacotes.component.ts
+++ src/app/pages/home/components/pacotes/pacotes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pacotes',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pacotes.component.html',
   styleUrl: './pacotes.component.css'
 })
